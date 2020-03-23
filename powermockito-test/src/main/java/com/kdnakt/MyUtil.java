0dcd8665d44977625d6d98c05c4b3a80ee2bbf77
kdnakt <a.kid.1985@gmail.com>
2020-03-24 08:51:24 +0900

    add isEqualOrBefore

--- powermockito-test/src/main/java/com/kdnakt/MyUtil.java
+++ powermockito-test/src/main/java/com/kdnakt/MyUtil.java
@@ -1,12 +1,17 @@
 package com.kdnakt;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.concurrent.TimeUnit;
 
 public class MyUtil {
 
     public static LocalDateTime now() {
         return LocalDateTime.now();
+    }
+
+    public static boolean isEqualOrBefore(final ZonedDateTime a, final ZonedDateTime b) {
+        return a.isEqual(b) || a.isBefore(b);
     }
 
     public static long timeToLive(final int hours) {
