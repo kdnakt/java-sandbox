a4e4497d5b4d2f49336bc603512596596a376427
kdnakt <a.kid.1985@gmail.com>
2021-04-10 20:10:58 +0900

    add javadoc project

---
+++ parent_project/01simple/src/main/java/com/kdnakt/simple/App.java
@@ -0,0 +1,9 @@
+package com.kdnakt.simple;
+
+public class App {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ parent_project/04originalBottomWithHtml/src/test/java/com/kdnakt/simple/AppTest.java
@@ -0,0 +1,20 @@
+package com.kdnakt.simple;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    private App app = new App();
+
+    @Test
+    public void testSimple() {
+        assertEquals(2, app.add(1, 1));
+    }
+
+    @Test
+    public void testEasy() {
+        assertEquals(2, app.add(2, 0));
+    }
+}
