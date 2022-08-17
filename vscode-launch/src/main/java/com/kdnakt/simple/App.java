cc7aeb75253d91456f5a866393244aa81c7ae429
kdnakt <a.kid.1985@gmail.com>
2022-08-17 23:03:26 +0900

    launch.json of VS Code

---
+++ vscode-launch/src/main/java/com/kdnakt/simple/App.java
@@ -0,0 +1,10 @@
+package com.kdnakt.simple;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("foo"));
+        System.out.println(System.getProperty("bar"));
+    }
+
+}
